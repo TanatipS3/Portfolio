43e2fa9ee9a1cef7dd6dd2ecdfa01e55dbb75e3a
TanatipS3 <150317844+TanatipS3@users.noreply.github.com>
2024-03-04 11:36:21 +0000

    add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Skills from './components/Skills/Skills'
+import Services from './components/Services/Services'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
      <Navbar/>
      <Hero/>
      <Skills/>
+     <Services/>
 
     </>
   )
